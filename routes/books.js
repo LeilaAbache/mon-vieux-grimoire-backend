03a2e37489f9f99bc0b24e827e2698532327ed50
Leila Abache <leila.aba@hotmail.fr>
2023-06-24 14:08:34 +0200

    correction finale

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,15 @@
 const express = require("express");
+// Création d'un routeur Express pour y enregistrer toutes les routes
 const router = express.Router();
+// Importation du module booksCtrl, contenant les fonctions de contrôleur pour gérer les différentes actions liées aux livres.
 const booksCtrl = require("../controllers/books");
-
+// Importation du module auth, contenant le middleware d'authentification pour sécuriser certaines routes
 const auth = require("../middleware/auth");
+// Importation du module sharpMulter, contenant le middleware de configuration pour le téléchargement d'images
 const sharpMulter = require("../middleware/image-config");
 
+// Les routes demandées sont créées à l'aide des méthodes du routeur
+// Chaque route est associée à une fonction de contrôleur spécifique
 router.get("/bestrating", booksCtrl.getBestRatedBooks);
 router.post("/", auth, sharpMulter, booksCtrl.createBook);
 router.get("/", booksCtrl.getAllBooks);
@@ -13,4 +18,6 @@ router.post("/:id/rating", auth, booksCtrl.createRating);
 router.put("/:id", auth, sharpMulter, booksCtrl.modifyBook);
 router.delete("/:id", auth, booksCtrl.deleteBook);
 
+// l'objet router est exporté en tant que module, permettant à d'autres fichiers de l'importer
+// et de l'utiliser pour gérer les routes associées aux livres dans l'application Express.
 module.exports = router;
